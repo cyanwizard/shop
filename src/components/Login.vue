da11fdcafa248c376d1a2036eb91120515cc8036
qingwu <394378132@qq.com>
2020-05-28 22:48:33 +0800

    添加部分登录功能

---
+++ src/components/Login.vue
@@ -0,0 +1,122 @@
+<template>
+  <div class="login-container">
+    <div class="login-box">
+      <!-- 头像区域 -->
+      <div class="avatar-box">
+        <img src="../assets/logo.png" alt />
+      </div>
+      <!-- 登录表单区域 -->
+      <el-form
+        label-width="0"
+        class="login-form"
+        :model="loginForm"
+        :rules="loginFormRules"
+        ref="loginFormRef"
+      >
+        <!-- 用户名 -->
+        <el-form-item prop="username">
+          <el-input v-model="loginForm.username" prefix-icon="iconfont icon-user"></el-input>
+        </el-form-item>
+        <!-- 密码 -->
+        <el-form-item prop="password">
+          <el-input
+            type="password"
+            v-model="loginForm.password"
+            prefix-icon="iconfont icon-3702mima"
+          ></el-input>
+        </el-form-item>
+        <!-- 按钮 -->
+        <el-form-item class="btns">
+          <el-button type="primary" @click="login">登录</el-button>
+          <el-button type="info" @click="resetLoginForm">重置</el-button>
+        </el-form-item>
+      </el-form>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      // 数据绑定
+      loginForm: {
+        username: 'abc',
+        password: '123'
+      },
+      //   验证规则
+      loginFormRules: {
+        username: [
+          { required: true, message: '请输入用户名', trigger: 'blur' },
+          { min: 3, max: 10, message: '长度在 3 到 10 个字符', trigger: 'blur' }
+        ],
+        password: [
+          { required: true, message: '请输入密码', trigger: 'blur' },
+          { min: 6, max: 15, message: '长度在 6 到 15 个字符', trigger: 'blur' }
+        ]
+      }
+    }
+  },
+  methods: {
+    // 重置表单
+    resetLoginForm () {
+      this.$refs.loginFormRef.resetFields()
+    },
+    // 发起请求前先验证合法性
+    login () {
+      this.$refs.loginFormRef.validate(valid => {
+        console.log(valid)
+      })
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+.login-container {
+  height: 100%;
+  background-color: #2b4b6b;
+}
+
+.login-box {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  width: 450px;
+  height: 300px;
+  background-color: #fff;
+  border-radius: 3px;
+  transform: translate(-50%, -50%);
+}
+
+.avatar-box {
+  position: absolute;
+  left: 50%;
+  width: 130px;
+  height: 130px;
+  border: 1px solid #eee;
+  padding: 10px;
+  background-color: #fff;
+  border-radius: 50%;
+  box-shadow: 0 0 10px #ddd;
+  transform: translate(-50%, -50%);
+  img {
+    width: 100%;
+    height: 100%;
+    background-color: #eee;
+    border-radius: 50%;
+  }
+}
+
+.login-form {
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  padding: 0 20px;
+  box-sizing: border-box;
+  .btns {
+    display: flex;
+    justify-content: flex-end;
+  }
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -2,6 +2,10 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import './plugins/element.js'
+// 引入全局样式表
+import './assets/css/global.css'
+// 引入字体图标
+import './assets/fonts/iconfont.css'
 
 Vue.config.productionTip = false
 
